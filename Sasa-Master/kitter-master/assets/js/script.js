19348f9d815e41e639e4be043d9ad1105414be2a
SuperiorDrey <132139152+Khaurie@users.noreply.github.com>
2024-10-09 02:44:20 +0800

    Add files via upload

---
+++ Sasa-Master/kitter-master/assets/js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
+
+function adjustButtonSize() {
+  const button = document.querySelector('.action-btn');
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth < 600) {
+    button.style.padding = '8px'; // Smaller padding for smaller screens
+    button.style.fontSize = '14px'; // Smaller font size
+  } else {
+    button.style.padding = '12px'; // Larger padding for larger screens
+    button.style.fontSize = '16px'; // Larger font size
+  }
+}
+
+// Adjust the button size on initial load
+adjustButtonSize();
+
+// Adjust the button size on window resize
+window.addEventListener('resize', adjustButtonSize);
